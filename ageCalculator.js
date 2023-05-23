f332b115a6dc6ded780bb06ca7d833ce032e0f56
Hualdev <hucastillo@gmail.com>
2023-05-22 19:29:48 -0500

    ageCalculate Final

--- ageCalculator.js
+++ ageCalculator.js
@@ -12,26 +12,64 @@ let today = new Date()
 let currentYear = today.getFullYear()
 let currentMonth = today.getMonth()+1
 let currentDay = today.getDate()
+const dayLabel = document.getElementById('dayLabel')
+const monthLabel = document.getElementById('monthLabel')
+const yearLabel = document.getElementById('yearLabel')
+
 
 day.addEventListener('keydown',()=>{
     if(day.value<=0 || day.value>31){
     dayError.style.display='inline'
+    dayLabel.style.color ='hsl(0, 100%, 67%)'
+    day.style.borderColor='hsl(0, 100%, 67%)'
     return
     }
     else{
         dayError.style.display='none'
+        dayLabel.style.color ='hsl(0, 1%, 44%)'
+        day.style.borderColor='hsl(0, 1%, 44%)'
+    }
+})
+
+month.addEventListener('keydown',()=>{
+    if(month.value<=0 || month.value>12){
+    monthError.style.display='inline'
+    monthLabel.style.color='hsl(0, 100%, 67%)'
+    month.style.borderColor='hsl(0, 100%, 67%)'
+    return
+    }
+    else{
+        monthError.style.display='none'
+        monthLabel.style.color='hsl(0, 1%, 44%)'
+        month.style.borderColor='hsl(0, 1%, 44%)'
+    }
+})
+
+year.addEventListener('keydown',()=>{
+    if((year.value-currentYear)>=0){
+    yearError.style.display='inline'
+    yearLabel.style.color='hsl(0, 100%, 67%)'
+    year.style.borderColor='hsl(0, 100%, 67%)'
+    }
+    else{
+        yearError.style.display='none'
+        yearLabel.style.color='hsl(0, 1%, 44%)'
+        year.style.borderColor='hsl(0, 1%, 44%)'
     }
 })
 
 arrow.addEventListener('click',()=>{
     if(year.value>currentYear || year.value===''){
         yearError.style.display='inline'
+        yearLabel.style.color='hsl(0, 100%, 67%)'
         return
-    }else if(year.value==currentYear && month.value>currentMonth || month.value==='' || month.value>12 || month.value<0){
+    }else if(year.value==currentYear && month.value>currentMonth || month.value==='' || month.value>12){
         monthError.style.display='inline'
+        monthLabel.style.color='hsl(0, 100%, 67%)'
         return
-    }else if(day.value>currentDay && month.value==currentMonth && year.value==currentYear || day.value==='' || day.value>31 || day.value<0){
+    }else if(day.value>currentDay && month.value==currentMonth && year.value==currentYear || day.value==='' || day.value>31){
         dayError.style.display='inline'
+        dayLabel.style.color ='hsl(0, 100%, 67%)'
         return
     }
 
@@ -44,6 +82,10 @@ arrow.addEventListener('click',()=>{
             months.textContent=currentMonth+12-(month.value)-1
             years.textContent=currentYear-(year.value)-1
            
+            if(currentYear<=year.value){
+                yearError.style.display='inline'
+                yearLabel.style.color='hsl(0, 100%, 67%)'
+            }
         }
         else{
             calculateDay()
@@ -78,31 +120,6 @@ function calculateDay(){
          days.textContent=currentDay+30-(day.value)
 }
 
-// day.addEventListener('keydown',()=>{
-//     if(day.value<=0 || day.value>31){
-//     dayError.style.display='inline'
-//     return
-//     }
-//     else{
-//         dayError.style.display='none'
-//     }
-// })
 
-// month.addEventListener('keydown',()=>{
-//     if(month.value<=0 || month.value>13){
-//     monthError.style.display='inline'
-//     return
-//     }
-//     else{
-//         monthError.style.display='none'
-//     }
-// })
 
-// year.addEventListener('keydown',()=>{
-//     if((year.value-currentYear)>=0){
-//     yearError.style.display='inline'
-//     }
-//     else{
-//         yearError.style.display='none'
-//     }
-// })
+
